63023281090815fb599622f433b6212029be56f0
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-11 16:14:57 +0530

    learning

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+
+    private students = [
+        {id : 1, name:"Pratibha", age: 22} ,
+        {id : 2, name:"Pragya", age: 24}
+    ];
+
+    getAllStudent() {
+        return this.students;
+    }
+
+    getStudentById( id : number) {
+        const student = this.students.find((s) => s.id === id);
+        if(!student) throw new NotFoundException('Student Not Found!');
+        return student;
+    }
+}
