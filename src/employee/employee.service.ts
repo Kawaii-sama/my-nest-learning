f68eee4ea691839a1e74d5541fc38fa26d25bd11
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-11 15:04:18 +0530

    learning controllers

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    getCategories(){
+        return ['Mobile', 'Tablet', 'Laptop'];
+    }
+}
