f26451d055ef75b23f3de604aaa11033a0f3ef16
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-13 03:37:22 +0530

    learning

---
+++ src/ev/ev.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { EvService } from './ev.service';
+
+@Controller('ev')
+export class EvController {
+    constructor (private readonly evService: EvService){}
+
+    @Get()
+    getUrl() {
+        return this.evService.getDbUrl();
+    }
+}
--- src/ev/ev.service.ts
+++ src/ev/ev.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigureService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/common';
 
 @Injectable()
 export class EvService {
-    constructor (private configureSerive: ConfigureService) {}
+    constructor (private configureSerive: ConfigService) {}
     getDbUrl(){
         return this.configureSerive.get<string>('DATABASE_URL')
     }
