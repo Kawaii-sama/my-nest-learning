c18881246a3e81f3b7a4e1313509e89bea1a9935
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-13 03:29:50 +0530

    learning

--- src/ev/ev.service.ts
+++ src/ev/ev.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigureService } from '@nestjs/common';
 
 @Injectable()
 export class EvService {
+    constructor (private configureSerive: ConfigureService) {}
     
 }
