6665cc5bec4b36770f2fc6148bee86e87d836d76
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-13 00:07:05 +0530

    learning

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, onModuleInit , onApplicationShutdown} from '@nestjs/common';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+    private isConnected = false;
+
+    onModuleInit(){
+        this.isConnected = true;
+        console.log(`Database Connected!`);
+    }
+
+    onApplicationShutdown(signal : string){
+        this.isConnected = false;
+        console.log(`Database Disconnected due to app shutdown. Signal ${signal}`)
+    }
+}
