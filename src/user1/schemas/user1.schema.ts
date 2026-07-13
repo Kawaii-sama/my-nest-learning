cda0d94a4ee9ab558a35f53971ec677857590aa6
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-13 20:48:01 +0530

    learning again

---
+++ src/user1/schemas/user1.schema.ts
@@ -0,0 +1,16 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose"
+import { Document } from "mongoose"
+import { Address } from "./address.schema"
+
+@Schema()
+export class User1 extends Document {
+    @Prop()
+    'name': string;
+
+    @Prop({ type : Address})
+    'address' : Address;
+
+
+}
+
+export const User1Schema = SchemaFactory.createForClass(User1);
--- src/user1/user1.module.ts
+++ src/user1/user1.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { User1Service } from './user1.service';
 import { User1Controller } from './user1.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User1, User1Schema } from './schemas/user1.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name : User1.name , schema: User1Schema}])],
   providers: [User1Service],
   controllers: [User1Controller]
 })
