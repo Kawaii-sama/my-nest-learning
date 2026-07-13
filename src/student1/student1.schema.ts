e2d388a6eec6b77cd308dcc4a376295589fbb1e6
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-13 17:06:04 +0530

    learning...

---
+++ src/student1/student1.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type Student1Document = Student1 & Document;
+
+@Schema({timestamps: true}) // two fields generated:- createdAt and updatedAt
+export class Student1 {
+    @Prop({required : true})
+    name: string;
+
+    @Prop({required : true})
+    age: number;
+
+    @Prop()
+    email?: string;
+}
+
+export const Student1Schema = SchemaFactory.createForClass(Student1);
