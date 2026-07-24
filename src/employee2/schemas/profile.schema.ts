109df197b95f7476c1951921ab7c20eb7337f284
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-24 19:15:26 +0530

    yay

--- src/employee2/schemas/profile.schema.ts
+++ src/employee2/schemas/profile.schema.ts
@@ -2,3 +2,13 @@ import {Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 import { Document } from "mongoose"
 
 @Schema()
+export class Profile extends Document {
+    @Prop()
+    'age' : number;
+
+    @Prop()
+    'qualifictaion' : string;
+
+
+
+}
