848d833e23a7db087c1eaf3702146c21a013f2e7
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-24 18:53:29 +0530

    yay

--- src/employee2/employee2.module.ts
+++ src/employee2/employee2.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { Employee2Service } from './employee2.service';
 
-@Module({})
+@Module({
+  providers: [Employee2Service]
+})
 export class Employee2Module {}
