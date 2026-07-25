e6600c57626cd9aeaa348d54fe0753a7288fbbad
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-25 16:03:47 +0530

    yay

--- src/employee2/employee2.service.ts
+++ src/employee2/employee2.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Employee2 } from './schemas/employee2.schema';
+import { Profile } from './schemas/profile.schema';
 
 @Injectable()
-export class Employee2Service {}
+export class Employee2Service {
+    constructor(
+        @InjectModel(Employee2.name) private employee2Model: Model <Employee2>,
+        @InjectModel(Profile.name) private profileModel: Model <Profile>
+    ) {}
+}
