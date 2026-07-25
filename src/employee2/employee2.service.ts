752e5b656668826e686f7fc7e96f6a5f3e33b0b3
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-25 16:41:18 +0530

    yay

--- src/employee2/employee2.service.ts
+++ src/employee2/employee2.service.ts
@@ -26,6 +26,6 @@ export class Employee2Service {
     }
 
     async findAll(): Promise <Employee2[]> {
-        return this.employee2Model.find().populate('Profile').exec();
+        return this.employee2Model.find().populate('profile').exec();
     }
 }
