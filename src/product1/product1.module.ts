fc9da723878201cbab62a0a5e2f943c2bedc04a5
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-25 17:22:54 +0530

    yay

--- src/product1/product1.module.ts
+++ src/product1/product1.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { Product1Service } from './product1.service';
+import { Product1Controller } from './product1.controller';
 
-@Module({})
+@Module({
+  providers: [Product1Service],
+  controllers: [Product1Controller]
+})
 export class Product1Module {}
