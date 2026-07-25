46ab6eb6b9665f20cb726d4f420174e9e04eaae0
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-25 16:37:03 +0530

    yay

--- src/employee2/employee2.controller.ts
+++ src/employee2/employee2.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { Employee2Service } from './employee2.service';
 
 @Controller('employee2')
-export class Employee2Controller {}
+export class Employee2Controller {
+
+    constructor(private readonly employee2service: Employee2Service) {}
+
+    @Post()
+    create() {
+        return this.employee2service.createEmployee2();
+    }
+}
